e55c3d347433d02499597e9fa205e9a472002bef
Bruno Mota <bmotadev@gmail.com>
2023-03-03 19:52:25 -0300

    :sparkles: feat: Testando Widget Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Teste',
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(fontWeight: FontWeight.bold),
+      textDirection: TextDirection.ltr,
+      textScaleFactor: 1.5,
+    );
+  }
+}
